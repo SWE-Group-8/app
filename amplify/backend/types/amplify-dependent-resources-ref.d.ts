36cf190ec6d145cbe542aa49c3c08f4f770c5195
Zaquariah Holland <zaquariah@gmail.com>
2022-03-09 21:14:34 -0600

    created initial app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "swegroup8c3e4f6f6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "swegroup8API": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo($owner: String) {
+    onCreateTodo(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo($owner: String) {
+    onUpdateTodo(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo($owner: String) {
+    onDeleteTodo(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote($owner: String) {
+    onCreateNote(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote($owner: String) {
+    onUpdateNote(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote($owner: String) {
+    onDeleteNote(owner: $owner) {
+      id
+      name
+      content
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
