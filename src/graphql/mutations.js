cd3c0ec258d122d9fa863728abf196205ce77725
Czechia7 <60117889+Czechia7@users.noreply.github.com>
2022-04-03 20:15:28 -0500

    GraphQL Things Done
    Completed Dans Inventory, graphql thingy

---
+++ src/graphql/mutations.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createDansInventory = /* GraphQL */ `
+  mutation CreateDansInventory(
+    $input: CreateDansInventoryInput!
+    $condition: ModelDansInventoryConditionInput
+  ) {
+    createDansInventory(input: $input, condition: $condition) {
+      id
+      name
+      color
+      price
+      fabric
+      type
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateDansInventory = /* GraphQL */ `
+  mutation UpdateDansInventory(
+    $input: UpdateDansInventoryInput!
+    $condition: ModelDansInventoryConditionInput
+  ) {
+    updateDansInventory(input: $input, condition: $condition) {
+      id
+      name
+      color
+      price
+      fabric
+      type
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteDansInventory = /* GraphQL */ `
+  mutation DeleteDansInventory(
+    $input: DeleteDansInventoryInput!
+    $condition: ModelDansInventoryConditionInput
+  ) {
+    deleteDansInventory(input: $input, condition: $condition) {
+      id
+      name
+      color
+      price
+      fabric
+      type
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDansInventory = /* GraphQL */ `
+  query GetDansInventory($id: ID!) {
+    getDansInventory(id: $id) {
+      id
+      name
+      color
+      price
+      fabric
+      type
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listDansInventories = /* GraphQL */ `
+  query ListDansInventories(
+    $filter: ModelDansInventoryFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDansInventories(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        color
+        price
+        fabric
+        type
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
